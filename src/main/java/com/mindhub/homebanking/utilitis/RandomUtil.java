e0f6dc4922fdf598adcf369d81cb643d302e271e
Ricardo Diaz <kerackdarcku@gmail.com>
2024-03-22 09:33:53 -0500

    test new method random

--- src/main/java/com/mindhub/homebanking/utilitis/RandomUtil.java
+++ src/main/java/com/mindhub/homebanking/utilitis/RandomUtil.java
@@ -9,13 +9,12 @@ public class RandomUtil {
         return "VIN-" + String.format("%0" + numDigits + "d", random.nextInt((int) Math.pow(10, numDigits)));
     }
 
-    public static int generateNumber(int numDigits) {
+    public static long generateNumber(int numDigits) {
         if (numDigits <= 0) {
             throw new IllegalArgumentException("Número de dígitos debe ser mayor que cero");
         }
 
-        int min = (int) Math.pow(10, numDigits - 1);
-        int max = (int) Math.pow(10, numDigits) - 1;
-        return min + random.nextInt(max - min + 1);
+        long range = (long) Math.pow(10, numDigits);
+        return random.nextLong(range) * (long) Math.pow(10, numDigits - 1);
     }
 }
