11dd75cac09c646a2f405dd267b5cae2ca2fb243
Ricardo Diaz <kerackdarcku@gmail.com>
2024-03-21 23:19:58 -0500

    debug randomutil

--- src/main/java/com/mindhub/homebanking/utilitis/RandomUtil.java
+++ src/main/java/com/mindhub/homebanking/utilitis/RandomUtil.java
@@ -1,13 +1,21 @@
 package com.mindhub.homebanking.utilitis;
 
+import java.util.Random;
+
 public class RandomUtil {
+    private static final Random random = new Random();
+
     public static String generateAccountNumber(int numDigits) {
-        return "VIN-" + String.format("%0" + numDigits + "d", new java.util.Random().nextInt((int) Math.pow(10, numDigits)));
+        return "VIN-" + String.format("%0" + numDigits + "d", random.nextInt((int) Math.pow(10, numDigits)));
     }
 
     public static long generateNumber(int numDigits) {
+        if (numDigits <= 0) {
+            throw new IllegalArgumentException("Número de dígitos debe ser mayor que cero");
+        }
+
         long min = (long) Math.pow(10, numDigits - 1);
         long max = (long) Math.pow(10, numDigits) - 1;
-        return min + new java.util.Random().nextLong(max - min + 1);
+        return min + random.nextLong(max - min + 1);
     }
 }
